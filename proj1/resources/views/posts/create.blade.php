c4a17f32c7295a2aa95776d9ea6784a9fa40b85f
Ivan Dronov <pogomgn@gmail.com>
2022-03-30 01:08:54 +0500

    laravelcollective/html

--- proj1/resources/views/posts/create.blade.php
+++ proj1/resources/views/posts/create.blade.php
@@ -2,11 +2,29 @@
 
 @section('content')
     <h2>Create post</h2>
+    {!! Form::open(['method' => 'POST', 'action' => 'App\Http\Controllers\PostController@store']) !!}
+    <div class="form-group">
+        {!! Form::label('title', 'Title') !!}
+        {!! Form::input('text', 'title') !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('url', 'Url') !!}
+        {!! Form::input('text', 'url') !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('content', 'Content') !!}
+        {!! Form::input('text', 'content') !!}
+    </div>
+    <div class="form-group">
+        {!! Form::submit() !!}
+    </div>
+    {!! Form::close() !!}
+
     <form method="post" action="/proj1/public/posts">
         @csrf
-        <input type="text" name="title" placeholder="Enter title" />
-        <input type="text" name="url" placeholder="Enter url" />
-        <input type="text" name="content" placeholder="Enter content" />
-        <input type="submit" name="submit" />
+        <input type="text" name="title" placeholder="Enter title"/>
+        <input type="text" name="url" placeholder="Enter url"/>
+        <input type="text" name="content" placeholder="Enter content"/>
+        <input type="submit" name="submit"/>
     </form>
 @endsection
